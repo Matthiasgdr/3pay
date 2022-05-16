5d1804cdb36fb38cb233e82b9874f589f1e519a0
RomainKH <romain.khanoyan@gmail.com>
2022-05-16 13:08:20 +0200

    feat: check siren

--- src/components/account/SignUp/index.jsx
+++ src/components/account/SignUp/index.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { useMoralis } from "react-moralis";
 import { Formik } from "formik";
+import { InputWrapper, Input, PasswordInput } from '@mantine/core';
+import axios from "axios";
 
 const SignUp = () => {
   const { Moralis } = useMoralis();
+  const config = {
+    headers: { Authorization: `Bearer ${process.env.REACT_APP_TOKEN_INSEE}` }
+  };
+  const checkSiren = (siren) => {
+    axios
+      .get("https://api.insee.fr/entreprises/sirene/V3/siren/" + siren, config)
+      .then(console.log)
+  }
+  checkSiren(853514149)
 
-  const handleSubmitSignup = async ({ username, email, password }) => {
+  const handleSubmitSignup = async ({ username, email, password, siren }) => {
     const user = new Moralis.User();
     user.set("username", username);
     user.set("email", email);
     user.set("password", password);
+    user.set("siren", siren);
 
     try {
       await user.signUp();
@@ -21,28 +33,33 @@ const SignUp = () => {
   return (
     <Formik
       onSubmit={handleSubmitSignup}
-      initialValues={{ username: "", email: "", password: "" }}
+      initialValues={{ username: "", email: "", password: "", siren: "" }}
     >
       {({ handleSubmit, handleChange, values }) => (
         <form onSubmit={handleSubmit}>
-          <input
-            name="username"
-            type="text"
-            onChange={handleChange}
-            value={values.username}
-          ></input>
-          <input
-            name="email"
-            type="email"
-            onChange={handleChange}
-            value={values.email}
-          ></input>
-          <input
-            name="password"
-            type="password"
-            onChange={handleChange}
-            value={values.password}
-          ></input>
+          <InputWrapper 
+            id="input-siren"
+            label="Numéro SIREN"
+            required
+            description="Veuillez indiquer le numéro de SIREN de votre entreprise pour créer votre compte"
+          >
+            <Input id="input-siren" onChange={handleChange} value={values.siren}/>
+          </InputWrapper>
+          <InputWrapper 
+            id="input-username"
+            label="Nom d'utilisateur"
+            required
+          >
+            <Input id="input-username" onChange={handleChange} value={values.username}/>
+          </InputWrapper>
+          <InputWrapper 
+            id="input-email"
+            label="Email"
+            required
+          >
+            <Input id="input-email" onChange={handleChange} value={values.email}/>
+          </InputWrapper>
+          <PasswordInput required label="Mot de passe" id="input-password" onChange={handleChange} value={values.password}/>
           <button type="submit">submit</button>
         </form>
       )}
