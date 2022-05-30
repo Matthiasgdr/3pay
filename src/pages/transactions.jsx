0a3f8b8ca9f0e4537e55d12b478b1da26f6b650e
RomainKH <romain.khanoyan@gmail.com>
2022-05-30 23:26:48 +0200

    feat: transactions in table

--- src/pages/transactions.jsx
+++ src/pages/transactions.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import useWalletTransactions from "../hooks/useWalletTransactions";
-import { useMoralis } from "react-moralis";
-import { Box, Title } from "@mantine/core";
+import { Box, Title, Table } from "@mantine/core";
+import { useUser } from "../hooks/useUser"
 
 const Transactions = () => {
-  const { Moralis } = useMoralis();
-  const currentUserAddress = Moralis.User.current().attributes.accounts;
+  const { user } = useUser()
+  const currentUserAddress = user.attributes.accounts;
+  function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+  }
+  
+  function formatDate(date) {
+    return [
+      padTo2Digits(date.getDate()),
+      padTo2Digits(date.getMonth() + 1),
+      date.getFullYear(),
+    ].join('/');
+  }
   
   const transactions = useWalletTransactions(currentUserAddress && currentUserAddress[0]);
-
   return (
     <Box>
       <Title order={1}
@@ -17,11 +27,24 @@ const Transactions = () => {
       >
         Historique de transactions
       </Title>
-      {transactions?.map((transaction) => (
-        <p key={transaction.id}>
-          {transaction.attributes.decimal.value.$numberDecimal} ETH
-        </p>
-      ))}
+      <Table>
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Prix</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions?.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.className}</td>
+              <td>{transaction.attributes.decimal.value.$numberDecimal}</td>
+              <td>{formatDate(transaction.attributes.createdAt)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Box>
   )
 }
